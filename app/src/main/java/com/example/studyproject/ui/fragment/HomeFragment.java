8ed448cacd64c98fa5414d4a5d973f9943b7d497
SPX0623 <1113532616@qq.com>
2022-02-28 10:35:45 +0800

    公茄大帅批

--- app/src/main/java/com/example/studyproject/ui/fragment/HomeFragment.java
+++ app/src/main/java/com/example/studyproject/ui/fragment/HomeFragment.java
@@ -1,24 +1,59 @@
 package com.example.studyproject.ui.fragment;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
-import com.example.studyproject.base.BaseFragment;
-import com.example.studyproject.databinding.HomeFragmentBinding;
+import com.example.studyproject.Bean.SmallPic;
+import com.example.studyproject.R;
+import com.example.studyproject.adapter.HorListview_Adapter;
+import com.example.studyproject.custom.HorizontalListView;
+
+import java.util.ArrayList;
 
 
 /**
  * @ClassName HomeFragment
- * @Author 史正龙
+ * @Author 孙培翔
  * @date 2022.02.25 15:49
  */
-public class HomeFragment extends BaseFragment<HomeFragmentBinding> {
+public class HomeFragment extends Fragment {
+    private ImageView ivChange;
+    private RelativeLayout relative;
+    private HorizontalListView horListview;
+
+//    @Override
+//    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+//        super.onViewCreated(view, savedInstanceState);
+//        binding.tv1.setText("");
+//    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.home_fragment, null);
+    }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        initView();
+        ArrayList<SmallPic> smallPics=new ArrayList<>();
+        smallPics.add(new SmallPic().setName("茄子").setUrl(R.drawable.gbl1));
+        HorListview_Adapter adapter=new HorListview_Adapter(smallPics,getActivity());
+        horListview.setAdapter(adapter);
+    }
+
+    private void initView() {
+        ivChange = (ImageView) getView().findViewById(R.id.iv_change);
+        relative = (RelativeLayout) getView().findViewById(R.id.relative);
+        horListview = (HorizontalListView) getView().findViewById(R.id.hor_listview);
     }
 }
