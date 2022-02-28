905b1d8447aff7982f593c2dbd64c4f077026fe2
szl <2890548298@qq.com>
2022-02-28 11:45:10 +0800

    second commit

--- app/src/main/java/com/example/studyproject/MainActivity.java
+++ app/src/main/java/com/example/studyproject/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.studyproject;
 
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -26,18 +30,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        verifyStoragePermissions(this);
         setFragmentMap();
         setFrameLayout(Objects.requireNonNull(fragmentMap.get("首页")));
 
         binding.mainBottom.setOnItemSelectedListener(item -> {
             int itemId = item.getItemId();
-            if (itemId == R.id.navigation_home) {
+            if (itemId == R.id.home_iv) {
                 setFrameLayout(Objects.requireNonNull(fragmentMap.get("首页")));
-            } else if (itemId == R.id.navigation_dashboard) {
+            } else if (itemId == R.id.gif_iv) {
                 setFrameLayout(Objects.requireNonNull(fragmentMap.get("Gif")));
             }
             return true;
         });
+    }
+
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            "android.permission.READ_EXTERNAL_STORAGE",
+            "android.permission.WRITE_EXTERNAL_STORAGE"};
+
+
+    public static void verifyStoragePermissions(Activity activity) {
+        try {
+            //检测是否有写的权限
+            int permission = ActivityCompat.checkSelfPermission(activity,
+                    "android.permission.WRITE_EXTERNAL_STORAGE");
+            if (permission != PackageManager.PERMISSION_GRANTED) {
+                // 没有写的权限，去申请写的权限，会弹出对话框
+                ActivityCompat.requestPermissions(activity, PERMISSIONS_STORAGE, REQUEST_EXTERNAL_STORAGE);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
--- app/src/main/java/com/example/studyproject/ui/activity/SplashActivity.java
+++ app/src/main/java/com/example/studyproject/ui/activity/SplashActivity.java
@@ -1,22 +1,25 @@
 package com.example.studyproject.ui.activity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 
 import com.bumptech.glide.Glide;
 import com.example.studyproject.MainActivity;
+import com.example.studyproject.R;
 import com.example.studyproject.base.BaseActivity;
 import com.example.studyproject.databinding.ActivitySpalshBinding;
 
 
+@SuppressLint("CustomSplashScreen")
 public class SplashActivity extends BaseActivity<ActivitySpalshBinding> {
     @Override
     public void init() {
         AlphaAnimation animation = new AlphaAnimation(0.3f, 1.0f);
         animation.setDuration(1000);
         //http://img5.adesk.com/61a1ba657e978a3b2dd42ae4?imageMogr2/thumbnail/!1080x1920r/gravity/Center/crop/1080x1920&sign=179454136412ce2020e6dbc448ec7315&t=62189e12
-        Glide.with(this).load("http://img5.adesk.com/61a1ba657e978a3b2dd42ae4?imageMogr2/thumbnail/!1080x1920r/gravity/Center/crop/1080x1920&sign=179454136412ce2020e6dbc448ec7315&t=62189e12")
+        Glide.with(this).load(R.mipmap.tp4)
                 .into(binding.splashIv);
         binding.splashIv.setAnimation(animation);
         animation.setAnimationListener(new Animation.AnimationListener() {
@@ -28,6 +31,7 @@ public class SplashActivity extends BaseActivity<ActivitySpalshBinding> {
             @Override
             public void onAnimationEnd(Animation animation) {
                 startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
             }
 
             @Override
