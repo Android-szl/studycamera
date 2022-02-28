b96b2feba608592412bc2d9a1e2476293a0aebf6
SPX0623 <1113532616@qq.com>
2022-02-28 10:45:13 +0800

    公茄大帅批2

---
+++ app/src/main/java/com/example/studyproject/Bean/SmallPic.java
@@ -0,0 +1,24 @@
+package com.example.studyproject.Bean;
+
+public class SmallPic {
+    private String name;
+    private int url;
+
+    public String getName() {
+        return name;
+    }
+
+    public SmallPic setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public int getUrl() {
+        return url;
+    }
+
+    public SmallPic setUrl(int url) {
+        this.url = url;
+        return this;
+    }
+}
